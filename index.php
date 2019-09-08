0c93934aecd4eb4bdb56acd0145d97674fe80095
Vadim <ardasovvadim@gmail.com>
2019-09-08 17:29:03 +0300

    check files

---
+++ index.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!--    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">-->
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/main.css?<?php echo time(); ?>">
+
+
+    <title>HMS</title>
+
+</head>
+<body>
+    <div class="container-fluid">
+
+<!--  /////////  HEAD  ///////  -->
+    <div class="head-section">
+        <div class="row justify-content-end up-contacts">
+            <p>г. Харьков</p>
+            <p class="number">+38 (095) 102 68 60</p>
+            <p class="text-grey">сегодня с 10.00 до 20.00</p>
+            <img src="img/point.svg">
+            <p class="text-grey up-address">ул. Благовещенская, д. 13 <br> м. Центральный Рынок</p>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-auto">
+                <img src="img/logo.svg">
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-auto t-logo">
+                Honey
+                <span class="t-bold">Music</span>
+                Studio
+            </div>
+        </div>
+        <div class="row justify-content-center nav-menu">
+            <div class="col-auto">
+                <a href="#">Про школу</a>
+                <a href="#">Курсы</a>
+                <a href="#">Цена</a>
+                <a href="#">Блог</a>
+                <a href="#">Контакты</a>
+            </div>
+        </div>
+    </div>
+
+        <!--    END HEAD    -->
+
+<!--   WHO WE ARE     -->
+
+        <div class="row">
+            <div class="col-6">
+                <img src="img/mainImg.png" alt="">
+            </div>
+            <div class="col-6">
+                <h1>Honey Music Studio</h1>
+                <h2>Кто мы</h2>
+                <p>Мы - коллектив профессиональных
+                    музыкантов, влюбленных в свое
+                    дело. Пожалуй, это главное, что
+                    нужно знать о Honey Music Studio.</p>
+                <button class="btn">Про школу</button>
+            </div>
+        </div>
+
+<!--    EXPERIENCE HMS    -->
+
+        <div class="row">
+            <div class="col-12">
+                <img src="img/experience.png" alt="">
+                <h1>Опыт HMS</h1>
+            </div>
+            <div class="col-12">
+                <h2>Наши резяльтаты в 2018/19 учебном году:</h2>
+            </div>
+            <div class="col-3">
+                <h2>15</h2>
+                <p>ярких концертов</p>
+            </div>
+            <div class="col-3">
+                <h2>12</h2>
+                <p>наград на конкурсах</p>
+            </div>
+            <div class="col-3">
+                <h2>3</h2>
+                <p>детских мюзыкла</p>
+            </div>
+            <div class="col-3">
+                <h2>2</h2>
+                <p>музыкальных мультфильма</p>
+            </div>
+            <div class="col-12">
+                <p>но нельзя измерить <span>цифрами</span> самое главное - вдохновение
+                    наших учеников, их <span>радость</span> и <span>блеск в глазах</span>.</p>
+            </div>
+        </div>
+
+<!--    COURSES    -->
+
+        <div class="row">
+            <div class="col-12">
+                <h1>Направления</h1>
+            </div>
+
+            <div class="col-6">
+                <h2>Фортепиано</h2>
+            </div>
+            <div class="col-3">
+                <div class="row">
+                    <div class="col-12"><h2>Вокал</h2></div>
+                    <div class="col-12"><h2>Струнные</h2></div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="row">
+                    <div class="col-12">
+                        <h2>Духовые</h2>
+                    </div>
+                    <div class="col-12">
+                        <h2>Ударные</h2>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-3">
+                <h2>Фортепиано Судзуки</h2>
+            </div>
+            <div class="col-3">
+                <h2>Музыкальная мультстудия</h2>
+            </div>
+            <div class="col-3">
+                <h2>Кавер-хор</h2>
+            </div>
+            <div class="col-3">
+                <h2>Общее музыкальное развитие</h2>
+            </div>
+            <div class="col-6">
+                <h2>Теория музыки</h2>
+            </div>
+            <div class="col-6">
+                <h2>Amadeus Camp</h2>
+            </div>
+        </div>
+
+
+    </div>
+
+    <!--  Optional Javascript  -->
+    <script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
