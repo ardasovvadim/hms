b4c738c6df59c1ce39b47f1b4c671eef0cd7ce13
ardasovvadim <ardasovvadim@gmail.com>
2020-01-30 00:56:10 +0200

    change php to pug

--- parts/bottom-scripts-include.php
+++
@@ -1,4 +0,0 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.js"></script>
-<!--<script src="../js/jquery-3.4.1.js"></script>-->
-<script src="../js/main.js?<?php echo time(); ?>"></script>
-
